bcdc8f64e0db714716d89777db5409baa750df07
Aurélien Grifasi <aurelien.grifasi@chronotruck.com>
2017-09-26 15:55:11 +0200

    Use FlagKit,

---
+++ CTKFlagPhoneNumber/CountryPicker/Country.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+struct Country {
+	var code: String?
+	var name: String?
+	var phoneCode: String?
+	var flag: UIImage? {
+		guard let code = self.code else { return nil }
+		return UIImage(named: code.uppercased(), in: Bundle.FlagIcons, compatibleWith: nil) ?? UIImage(named: "unknown", in: Bundle.CTKFlagPhoneNumber(), compatibleWith: nil)
+	}
+	
+	init(code: String?, name: String?, phoneCode: String?) {
+		self.code = code
+		self.name = name
+		self.phoneCode = phoneCode
+	}
+}
---
+++ pushPod.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+pod lib lint
+pod trunk push CTKFlagPhoneNumber.podspec
