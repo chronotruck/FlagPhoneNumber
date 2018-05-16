d3a0f86a7859b2db9addedfb2ff9d6953df2d4bc
Aurélien Grifasi <aurelien.grifasi@chronotruck.com>
2018-05-16 11:12:50 +0200

    add more checks

--- CTKFlagPhoneNumber/CountryPicker/Country.swift
+++ CTKFlagPhoneNumber/CountryPicker/Country.swift
@@ -6,7 +6,7 @@ struct Country {
 	var phoneCode: String?
 	var flag: UIImage? {
 		guard let code = self.code else { return nil }
-		return UIImage(named: code.uppercased(), in: Bundle.FlagIcons, compatibleWith: nil) ?? UIImage(named: "unknown", in: Bundle.FlagIcons, compatibleWith: nil)
+		return UIImage(named: code, in: Bundle.FlagIcons, compatibleWith: nil) ?? UIImage(named: "unknown", in: Bundle.FlagIcons, compatibleWith: nil)
 	}
 	
 	init(code: String?, name: String?, phoneCode: String?) {
