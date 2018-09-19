aab084de9e78d49a4732a9398859ef4e2c3d7ad3
Aurélien Grifasi <aurelien.grifasi@chronotruck.com>
2018-09-19 11:16:39 +0200

    🚨 clean project

--- FlagPhoneNumber/FPNCountryPicker/FPNCountryPicker.swift
+++ FlagPhoneNumber/FPNCountryPicker/FPNCountryPicker.swift
@@ -1,43 +1,43 @@
 import UIKit
 
 open class FPNCountryPicker: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSource {
-	
+
 	var countries: [FPNCountry]!
 	open var selectedLocale: Locale?
 	weak var countryPickerDelegate: FPNCountryPickerDelegate?
 	open var showPhoneNumbers: Bool = true
-		
+
 	override init(frame: CGRect) {
 		super.init(frame: frame)
-		
+
 		setup()
 	}
-	
+
 	required public init?(coder aDecoder: NSCoder) {
 		super.init(coder: aDecoder)
-		
+
 		setup()
 	}
-	
+
 	func setup() {
 		if let code = Locale.preferredLanguages.first {
 			self.selectedLocale = Locale(identifier: code)
 		}
-		
+
 		countries = countryNamesByCode()
-		
+
 		super.dataSource = self
 		super.delegate = self
 	}
-	
+
 	// MARK: - Locale Methods
-	
+
 	open func setLocale(_ locale: String) {
 		self.selectedLocale = Locale(identifier: locale)
 	}
-	
+
 	// MARK: - FPNCountry Methods
-	
+
 	open func setCountry(_ code: String) {
 		for index in 0..<countries.count {
 			if countries[index].code == code {
@@ -45,7 +45,7 @@ open class FPNCountryPicker: UIPickerView, UIPickerViewDelegate, UIPickerViewDat
 			}
 		}
 	}
-	
+
 	open func setCountryByPhoneCode(_ phoneCode: String) {
 		for index in 0..<countries.count {
 			if countries[index].phoneCode == phoneCode {
@@ -53,7 +53,7 @@ open class FPNCountryPicker: UIPickerView, UIPickerViewDelegate, UIPickerViewDat
 			}
 		}
 	}
-	
+
 	open func setCountryByName(_ name: String) {
 		for index in 0..<countries.count {
 			if countries[index].name == name {
@@ -61,19 +61,19 @@ open class FPNCountryPicker: UIPickerView, UIPickerViewDelegate, UIPickerViewDat
 			}
 		}
 	}
-	
+
 	func setCountryByRow(row: Int) {
 		self.selectRow(row, inComponent: 0, animated: true)
-		
+
 		if countries.count > 0 {
 			let country = countries[row]
 
 			countryPickerDelegate?.countryPhoneCodePicker(self, didSelectCountry: country)
 		}
 	}
-	
+
 	// Populates the metadata from the included json file resource
-	
+
 	private func countryNamesByCode() -> [FPNCountry] {
 		let bundle: Bundle = Bundle.FlagPhoneNumber()
 		let resource: String = "countryCodes"
@@ -82,69 +82,69 @@ open class FPNCountryPicker: UIPickerView, UIPickerViewDelegate, UIPickerViewDat
 		assert(jsonPath != nil, "Resource file is not found in the Bundle")
 
 		let jsonData = try? Data(contentsOf: URL(fileURLWithPath: jsonPath!))
-		
+
 		assert(jsonPath != nil, "Resource file is not found")
-		
+
 		var countries = [FPNCountry]()
-		
+
 		do {
 			if let jsonObjects = try JSONSerialization.jsonObject(with: jsonData!, options: JSONSerialization.ReadingOptions.allowFragments) as? NSArray {
-				
+
 				for jsonObject in jsonObjects {
-					
+
 					guard let countryObj = jsonObject as? NSDictionary else {
 						return countries
 					}
-					
+
 					guard let code = countryObj["code"] as? String, let phoneCode = countryObj["dial_code"] as? String, let name = countryObj["name"] as? String else {
 						return countries
 					}
-					
+
 					if let locale = self.selectedLocale {
 						let country = FPNCountry(code: code, name: locale.localizedString(forRegionCode: code), phoneCode: phoneCode)
 
 						countries.append(country)
 					} else {
 						let country = FPNCountry(code: code, name: name, phoneCode: phoneCode)
-						
+
 						countries.append(country)
 					}
 				}
-				
+
 			}
 		} catch let error {
 			assertionFailure(error.localizedDescription)
 		}
 		return countries.sorted(by: { $0.name! < $1.name! })
 	}
-		
+
 	// MARK: - Picker Methods
-	
+
 	open func numberOfComponents(in pickerView: UIPickerView) -> Int {
 		return 1
 	}
-	
+
 	open func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
 		return countries.count
 	}
-	
+
 	open func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
 		var resultView: FPNCountryView
-		
+
 		if view == nil {
 			resultView = FPNCountryView()
 		} else {
 			resultView = view as! FPNCountryView
 		}
-		
+
 		resultView.setup(countries[row])
-		
+
 		if !showPhoneNumbers {
 			resultView.countryCodeLabel.isHidden = true
 		}
 		return resultView
 	}
-	
+
 	open func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
 		if countries.count > 0 {
 			let country = countries[row]
--- FlagPhoneNumber/FPNSearchCountryViewController.swift
+++ FlagPhoneNumber/FPNSearchCountryViewController.swift
@@ -9,30 +9,29 @@
 import Foundation
 
 class FPNSearchCountryViewController: UITableViewController, UISearchResultsUpdating, UISearchControllerDelegate {
-	
+
 	var searchController: UISearchController?
 	var list: [FPNCountry]?
 	var results: [FPNCountry]?
-	
+
 	var delegate: FPNDelegate?
-	
-	
+
 	init(countries: [FPNCountry]) {
 		super.init(nibName: nil, bundle: nil)
-		
+
 		self.list = countries
 	}
-	
+
 	required init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
-	
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+
 		initSearchBarController()
 	}
-	
+
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 
@@ -42,57 +41,57 @@ class FPNSearchCountryViewController: UITableViewController, UISearchResultsUpda
 			// Fallback on earlier versions
 		}
 	}
-	
+
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
-		
+
 		searchController?.isActive = true
 	}
-	
+
 	@objc private func dismissController() {
 		dismiss(animated: true, completion: nil)
 	}
-	
+
 	private func initSearchBarController() {
 		searchController = UISearchController(searchResultsController: nil)
 		searchController?.searchResultsUpdater = self
 		searchController?.delegate = self
-		
+
 		if #available(iOS 9.1, *) {
 			searchController?.obscuresBackgroundDuringPresentation = false
 		} else {
 			// Fallback on earlier versions
 		}
-		
+
 		if #available(iOS 11.0, *) {
 			navigationItem.searchController = searchController
 		} else {
 			searchController?.dimsBackgroundDuringPresentation = false
 			searchController?.hidesNavigationBarDuringPresentation = true
 			searchController?.definesPresentationContext = true
-			
+
 			//				searchController?.searchBar.sizeToFit()
 			tableView.tableHeaderView = searchController?.searchBar
 		}
 		definesPresentationContext = true
 	}
-	
+
 	private func getItem(at indexPath: IndexPath) -> FPNCountry {
 		var array: [FPNCountry]!
-		
+
 		if let searchController = searchController, searchController.isActive && results != nil && results!.count > 0 {
 			array = results
 		} else {
 			array = list
 		}
-		
+
 		return array[indexPath.row]
 	}
-	
+
 	override func numberOfSections(in tableView: UITableView) -> Int {
 		return 1
 	}
-	
+
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		if let searchController = searchController, searchController.isActive {
 			if let count = searchController.searchBar.text?.count, count > 0 {
@@ -101,18 +100,18 @@ class FPNSearchCountryViewController: UITableViewController, UISearchResultsUpda
 		}
 		return list?.count ?? 0
 	}
-	
+
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
 		let country = getItem(at: indexPath)
-		
+
 		cell.textLabel?.text = country.name
 		cell.detailTextLabel?.text = country.phoneCode
 		cell.imageView?.image = country.flag
-		
+
 		return cell
 	}
-	
+
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		tableView.deselectRow(at: indexPath, animated: true)
 
@@ -123,7 +122,7 @@ class FPNSearchCountryViewController: UITableViewController, UISearchResultsUpda
 	}
 
 	// UISearchResultsUpdating
-	
+
 	func updateSearchResults(for searchController: UISearchController) {
 		if list == nil {
 			results?.removeAll()
@@ -133,7 +132,7 @@ class FPNSearchCountryViewController: UITableViewController, UISearchResultsUpda
 			tableView.reloadData()
 			return
 		}
-		
+
 		if let searchText = searchController.searchBar.text, searchText.count > 0 {
 			results = list!.filter({(item: FPNCountry) -> Bool in
 				if item.name?.lowercased().range(of: searchText.lowercased()) != nil {
@@ -150,17 +149,17 @@ class FPNSearchCountryViewController: UITableViewController, UISearchResultsUpda
 	}
 
 	// UISearchControllerDelegate
-	
+
 	func didPresentSearchController(_ searchController: UISearchController) {
 		DispatchQueue.main.async { [unowned self] in
 			self.searchController?.searchBar.becomeFirstResponder()
 		}
 	}
-	
+
 	func willDismissSearchController(_ searchController: UISearchController) {
 		results?.removeAll()
 	}
-	
+
 	func didDismissSearchController(_ searchController: UISearchController) {
 		dismissController()
 	}
