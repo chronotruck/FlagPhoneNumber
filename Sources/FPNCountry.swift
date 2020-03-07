b1dd882203a56d3a09a31373a4622658fba65220
Carlos Monzon <carlos@prezzee.com.au>
2020-03-07 22:55:55 +1100

    remove initialisation of the flag UIImage at FPNCountry and make it computer property

--- Sources/FPNCountry.swift
+++ Sources/FPNCountry.swift
@@ -2,24 +2,31 @@ import UIKit
 
 public struct FPNCountry: Equatable {
 
-	public var code: FPNCountryCode
+    public var code: FPNCountryCode {
+        get {
+            FPNCountryCode(rawValue: self.codeStr)!
+        }
+    }
+    private var codeStr: String
 	public var name: String
 	public var phoneCode: String
-	var flag: UIImage?
+	var flag: UIImage? {
+        get {
+            if let flag = UIImage(named: self.codeStr, in: Bundle.FlagIcons, compatibleWith: nil) {
+                return flag
+            } else {
+                return UIImage(named: "unknown", in: Bundle.FlagIcons, compatibleWith: nil)
+            }
+        }
+    }
 
 	init(code: String, name: String, phoneCode: String) {
 		self.name = name
 		self.phoneCode = phoneCode
-		self.code = FPNCountryCode(rawValue: code)!
-
-		if let flag = UIImage(named: code, in: Bundle.FlagIcons, compatibleWith: nil) {
-			self.flag = flag
-		} else {
-			self.flag = UIImage(named: "unknown", in: Bundle.FlagIcons, compatibleWith: nil)
-		}
+        self.codeStr = code
 	}
-
-	static public func ==(lhs: FPNCountry, rhs: FPNCountry) -> Bool {
+    
+	static public func == (lhs: FPNCountry, rhs: FPNCountry) -> Bool {
 		return lhs.code == rhs.code
 	}
 }
